a2ee7e763b530054091a71705d1a0bdd72749238
CL4-Bisk <jcaparicio@up.edu.ph>
2026-05-27 10:23:10 +0800

    feat: implemented initial functions of Access class
    Attributes: None
    Constructor: None
    
    Each function has its corresponding set of def, param, process, and return values

---
+++ addressing.py
@@ -0,0 +1,10 @@
+from storage import memory, register, variable
+from bin_convert import HalfPrecision
+
+class Access:
+    @staticmethod
+    def data(addr, flow):
+        pass
+    @staticmethod
+    def store(typ, addr, value):
+        pass
