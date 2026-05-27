66fd2c73017eaf683f29f9a903078abe425bc88f
CL4-Bisk <jcaparicio@up.edu.ph>
2026-05-27 10:32:46 +0800

    feat: implemented initial functions of AddressingMode class

--- addressing.py
+++ addressing.py
@@ -8,3 +8,44 @@ class Access:
     @staticmethod
     def store(typ, addr, value):
         pass
+
+class AddressingMode:
+    @staticmethod
+    def immediate(var):
+        pass
+    
+    @staticmethod
+    def relative(displace):
+        pass
+    
+    @staticmethod
+    def based(displace):
+        pass
+    
+    @staticmethod
+    def indexed(displace):
+        pass
+    
+    @staticmethod
+    def register(reg_addr):
+        pass
+    
+    @staticmethod
+    def register_indirect(reg_addr):
+        pass
+    
+    @staticmethod
+    def direct(var_addr):
+        pass
+    
+    @staticmethod
+    def indirect(var_addr):
+        pass
+    
+    @staticmethod
+    def autoinc(reg_addr):
+        pass
+    
+    @staticmethod
+    def autodec(reg_addr):
+        pass
